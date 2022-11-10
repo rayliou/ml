5381a2b0cb5fd2d10ca1f30fd9b728b54472e480
henry <xrayliou@gmail.com>
2022-11-10 11:41:48 -0500

    modify python setup

--- python/01_py3713_install.sh
+++ python/01_py3713_install.sh
@@ -4,11 +4,11 @@ set -ex
 download () {
 	wget "https://www.python.org/ftp/python/3.7.13/Python-3.7.13.tgz"
 	tar -zxvf Python-3.7.13.tgz
-	cd  Python-3.7.13
 }
 build () {
 	cd  Python-3.7.13
 	sudo  mkdir -p  /home/python-3.7.13 && sudo ln -sf  /home/python-3.7.13 /usr/local/
+    sudo apt install  -y libsqlite3-dev
 	./configure --prefix=/home/python-3.7.13/ --with-ssl-default-suites=openssl --enable-loadable-sqlite-extensions  && make -j16
 	sudo make install
 
@@ -17,6 +17,10 @@ clean () {
     sudo rm -fr  Python-3.7.13 Python-3.7.13.tgz
 }
 
+
+
+download
+build
 
 
 #build
