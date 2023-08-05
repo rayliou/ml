6da2ed3ce9ab19b32f118c975f1a7ba9bf56d231
Xiaorui Liu <xiaorui.liu@fluent.ai>
2023-08-04 20:45:40 -0400

    web audio

--- cxx/ut/ray_ut.h
+++ cxx/ut/ray_ut.h
@@ -4,7 +4,11 @@
 #include <cstdio>
 
 #define REQUIRE(cond) if (!(cond)) {\
-    __ut_ret__ = false; printf("%s:%d: Failed: %s\n", __FILE__, __LINE__, #cond); return;\
+    __ut_ret__ = false; printf("[REQUIRE] %s:%d: Failed: %s\n", __FILE__, __LINE__, #cond); return;\
+}
+
+#define CHECK(cond) if (!(cond)) {\
+    __ut_ret__ = false; printf("[CHECK]%s:%d: Failed: %s\n", __FILE__, __LINE__, #cond);\
 }
 
 #define TEST_CASE(name) TEST_CASE_(name, __COUNTER__)
@@ -103,4 +107,33 @@ int main(int argc, char* argv[]) {
 
 TEST_CASE("My first test") {
     REQUIRE(1 == 1);
+    CHECK(1 != 1);
+    CHECK(2 == 2);
+}
+
+//FYI: What common algorithms are used for C's rand()? : 
+//  https://stackoverflow.com/questions/1026327/what-common-algorithms-are-used-for-cs-rand
+//  Use a faster old version glibc rand number generator
+//
+//  Set seed as 1678717964 which is the seconds since 1970-01-01 00:00:00 UTC to  Mon 13 Mar 2023 10:32:44 AM EDT
+static unsigned int rnd_seed_next = 1678717964;
+static const auto FLUENTAI_RAND_MAX = (unsigned long long)(1UL << 31);
+inline int rand_r (/*unsigned int *seed*/)
+{
+  int result;
+
+  rnd_seed_next *= 1103515245;
+  rnd_seed_next += 12345;
+  result = (unsigned int) (rnd_seed_next / 65536) % 2048;
+
+  rnd_seed_next *= 1103515245;
+  rnd_seed_next += 12345;
+  result <<= 10;
+  result ^= (unsigned int) (rnd_seed_next / 65536) % 1024;
+
+  rnd_seed_next *= 1103515245;
+  rnd_seed_next += 12345;
+  result <<= 10;
+  result ^= (unsigned int) (rnd_seed_next / 65536) % 1024;
+  return result;
 }
